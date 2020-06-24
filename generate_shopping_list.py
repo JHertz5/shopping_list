a417a1a5972acffc5f2d64faf6afe1ce5c1eb868
jhertz5 <jukka.hertzog@live.co.uk>
2020-06-24 22:04:58 +0100

    Changed generate_shopping_list.py and update_macros.py to be functions instead of plain scripts for more flexiiblity.

--- generate_shopping_list.py
+++ generate_shopping_list.py
@@ -1,84 +1,89 @@
+''' function to generate shopping list '''
+
 import sheet_interface
 import shopping_list_manager
 import checklist_manager
 import push_file
 import os # for deleting file
 
-sheets = sheet_interface.open_spreadsheet()
-print('data connected')
+def generate_shopping_list():
+    sheets = sheet_interface.open_spreadsheet()
+    print('data connected')
 
-items_sheet = sheets.worksheet('Items')
-item_groups = sheet_interface.get_data_item_group(items_sheet)
-print('items retrieved')
+    items_sheet = sheets.worksheet('Items')
+    item_groups = sheet_interface.get_data_item_group(items_sheet)
+    print('items retrieved')
 
-# extract data from recipe sheet
-recipes_sheet = sheets.worksheet('Recipes')
-recipes = sheet_interface.get_data_recipes(recipes_sheet)
-print('recipes retrieved')
+    # extract data from recipe sheet
+    recipes_sheet = sheets.worksheet('Recipes')
+    recipes = sheet_interface.get_data_recipes(recipes_sheet)
+    print('recipes retrieved')
 
-generate_list = True
-while generate_list:
-    # extract data from input sheet
-    input_sheet = sheets.worksheet('Input')
-    (meals_to_buy, exclusions_list, inclusions_list) = sheet_interface.get_data_input(input_sheet)
-    exclusions = set(exclusions_list)
-    inclusions = set(inclusions_list)
-    print('input data retrieved')
-    print('data retrieved')
+    generate_list = True
+    while generate_list:
+        # extract data from input sheet
+        input_sheet = sheets.worksheet('Input')
+        (meals_to_buy, exclusions_list, inclusions_list) = sheet_interface.get_data_input(input_sheet)
+        exclusions = set(exclusions_list)
+        inclusions = set(inclusions_list)
+        print('input data retrieved')
+        print('data retrieved')
 
-    # create list of recipes containing only recipes to go in shopping list
-    recipes_to_buy = [ recipes[x] for x in meals_to_buy ]
+        # create list of recipes containing only recipes to go in shopping list
+        recipes_to_buy = [ recipes[x] for x in meals_to_buy ]
 
-    # instantiate shopping list and add recipes
-    shopping_list = shopping_list_manager.ShoppingList()
-    for recipe, ingredients in zip(meals_to_buy,recipes_to_buy):
-        shopping_list.add_recipe(recipe, ingredients)
+        # instantiate shopping list and add recipes
+        shopping_list = shopping_list_manager.ShoppingList()
+        for recipe, ingredients in zip(meals_to_buy,recipes_to_buy):
+            shopping_list.add_recipe(recipe, ingredients)
 
-    # inclusions overrides exclusions
-    excluded_items = exclusions.difference(inclusions)
-    shopping_list.exclude_items(excluded_items)
-    shopping_list.include_items(inclusions)
+        # inclusions overrides exclusions
+        excluded_items = exclusions.difference(inclusions)
+        shopping_list.exclude_items(excluded_items)
+        shopping_list.include_items(inclusions)
 
-    # assigning groups to shopping list items
-    shopping_list.set_item_groups(item_groups)
+        # assigning groups to shopping list items
+        shopping_list.set_item_groups(item_groups)
 
-    shopping_list.preview_list()
-    print('shopping list generated')
+        shopping_list.preview_list()
+        print('shopping list generated')
 
-    send_list_question = '[s]end list, [r]efresh list or [q]uit?\n'
-    send_list_responses = ['s','r','q']
-    # ask user whether to push shopping list
-    send_list_input = input(send_list_question)
-    while send_list_input not in send_list_responses:
-        print('{} is not valid input {}'.format(
-            send_list_input, send_list_responses))
+        send_list_question = '[s]end list, [r]efresh list or [q]uit?\n'
+        send_list_responses = ['s','r','q']
+        # ask user whether to push shopping list
         send_list_input = input(send_list_question)
+        while send_list_input not in send_list_responses:
+            print('{} is not valid input {}'.format(
+                send_list_input, send_list_responses))
+            send_list_input = input(send_list_question)
 
-    if send_list_input == 's': # send file
-        print('send file selected')
+        if send_list_input == 's': # send file
+            print('send file selected')
 
-        # generate grouped list
-        shopping_list_grouped = shopping_list.generate_grouped_list()
+            # generate grouped list
+            shopping_list_grouped = shopping_list.generate_grouped_list()
 
-        # generate checklist file
-        checklist = checklist_manager.checklist_manager()
-        checklist.generate_file(shopping_list_grouped)
-        checklist_filename = checklist.filename
-        print('{} generated'.format(checklist_filename))
+            # generate checklist file
+            checklist = checklist_manager.checklist_manager()
+            checklist.generate_file(shopping_list_grouped)
+            checklist_filename = checklist.filename
+            print('{} generated'.format(checklist_filename))
 
-        # push file
-        push_file.push_file(checklist_filename)
-        print('file pushed')
+            # push file
+            push_file.push_file(checklist_filename)
+            print('file pushed')
 
-        os.remove(checklist_filename) # delete file
+            os.remove(checklist_filename) # delete file
 
-        generate_list = False; # end script
+            generate_list = False; # end script
 
-    elif send_list_input == 'r': # refresh file
-        print('refresh file selected')
-        generate_list = True; # restart script
+        elif send_list_input == 'r': # refresh file
+            print('refresh file selected')
+            generate_list = True; # restart script
 
-    else:
-        print('quit selected')
-        generate_list = False; # end script
-print('exiting script')
+        else:
+            print('quit selected')
+            generate_list = False; # end script
+
+if __name__ == "__main__":
+    generate_shopping_list()
--- update_macros.py
+++ update_macros.py
@@ -1,72 +1,75 @@
-''' script to calculate macros in meals based with google sheets as database '''
+''' function to calculate macros in each recipe '''
 
 import sheet_interface
 
-sheets = sheet_interface.open_spreadsheet()
+def update_macros():
+    sheets = sheet_interface.open_spreadsheet()
 
+    # get ingredients data into dict of dicts
+    ingredient_macros_sheet = sheets.worksheet('Items-Macros')
+    ingredient_macros_raw = ingredient_macros_sheet.get_all_records()
+    ingredient_macros = {}
+    for ingredient in ingredient_macros_raw:
+        ingredient_name = ingredient.pop('Item')
+        ingredient_macros[ingredient_name] = ingredient
 
-# get ingredients data into dict of dicts
-ingredient_macros_sheet = sheets.worksheet('Items-Macros')
-ingredient_macros_raw = ingredient_macros_sheet.get_all_records()
-ingredient_macros = {}
-for ingredient in ingredient_macros_raw:
-    ingredient_name = ingredient.pop('Item')
-    ingredient_macros[ingredient_name] = ingredient
+    # get meal ingredients into dict of lists
+    meal_ingredients_sheet = sheets.worksheet('Recipes')
+    meal_ingredients_raw = meal_ingredients_sheet.get_all_values()
+    meal_ingredients = {}
+    for meal in meal_ingredients_raw:
+        meal_name = meal.pop(0)
+        meal_ingredients[meal_name] = [ x for x in meal if x != '' ]
 
-# get meal ingredients into dict of lists
-meal_ingredients_sheet = sheets.worksheet('Recipes')
-meal_ingredients_raw = meal_ingredients_sheet.get_all_values()
-meal_ingredients = {}
-for meal in meal_ingredients_raw:
-    meal_name = meal.pop(0)
-    meal_ingredients[meal_name] = [ x for x in meal if x != '' ]
+    # get headings for table fo meal macros
+    arbitrary_ingredient = next(iter(ingredient_macros))
+    macro_headings_raw = ingredient_macros[arbitrary_ingredient].keys()
+    macro_headings = [ x for x in macro_headings_raw if '/Serving' in x ]
 
-# get headings for table fo meal macros
-arbitrary_ingredient = next(iter(ingredient_macros))
-macro_headings_raw = ingredient_macros[arbitrary_ingredient].keys()
-macro_headings = [ x for x in macro_headings_raw if '/Serving' in x ]
+    print('data retrieved')
 
-print('data retrieved')
+    # get list of meals
+    meal_names = sorted(list(meal_ingredients.keys()))
 
-# get list of meals
-meal_names = sorted(list(meal_ingredients.keys()))
+    # get cells from recipe_macros_sheet
+    recipe_macros_sheet = sheets.worksheet('Recipes-Macros')
+    cell_range = 'A1:F' + str(len(meal_names)+1)
+    cell_list = recipe_macros_sheet.range(cell_range)
 
-# get cells from recipe_macros_sheet
-recipe_macros_sheet = sheets.worksheet('Recipes-Macros')
-cell_range = 'A1:F' + str(len(meal_names)+1)
-cell_list = recipe_macros_sheet.range(cell_range)
-
-# fill in heading names
-cell_counter = 0
-cell_list[cell_counter].value = 'Meal'
-for index,cell in enumerate( cell_list[ 1 : 1+len(macro_headings) ]):
-    cell_counter += 1
-    cell.value = macro_headings[index]
-cell_counter += 1
-cell_list[cell_counter].value = 'Skipped Items'
-
-for meal in meal_names:
-    # fill in name of meal
-    cell_counter += 1
-    cell_list[cell_counter].value = meal
-
-    # find total of all ingredients for each heading and write to cell
-    for heading in macro_headings:
+    # fill in heading names
+    cell_counter = 0
+    cell_list[cell_counter].value = 'Meal'
+    for index,cell in enumerate( cell_list[ 1 : 1+len(macro_headings) ]):
         cell_counter += 1
-        ingredient_macroList = [ int(ingredient_macros[x][heading])
-            for x in meal_ingredients[meal] if x in ingredient_macros]
-        mealMacroTotal = sum(ingredient_macroList)
-        cell_list[cell_counter].value = mealMacroTotal
-
+        cell.value = macro_headings[index]
     cell_counter += 1
-    skipped_ingredients = [ x for x in meal_ingredients[meal]
-        if x not in ingredient_macros ]
-    cell_list[cell_counter].value = ', '.join(skipped_ingredients)
+    cell_list[cell_counter].value = 'Skipped Items'
+
+    for meal in meal_names:
+        # fill in name of meal
+        cell_counter += 1
+        cell_list[cell_counter].value = meal
+
+        # find total of all ingredients for each heading and write to cell
+        for heading in macro_headings:
+            cell_counter += 1
+            ingredient_macroList = [ int(ingredient_macros[x][heading])
+                for x in meal_ingredients[meal] if x in ingredient_macros]
+            mealMacroTotal = sum(ingredient_macroList)
+            cell_list[cell_counter].value = mealMacroTotal
+
+        cell_counter += 1
+        skipped_ingredients = [ x for x in meal_ingredients[meal]
+            if x not in ingredient_macros ]
+        cell_list[cell_counter].value = ', '.join(skipped_ingredients)
 
 
-print('totals calculated')
+    print('totals calculated')
 
-recipe_macros_sheet.update_cells(cell_list)
+    recipe_macros_sheet.update_cells(cell_list)
 
-print('new data written')
-print('macro update complete')
+    print('new data written')
+    print('macro update complete')
+
+if __name__ == "__main__":
+    update_macros()
