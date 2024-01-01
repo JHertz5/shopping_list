dc526e2747b261c1d671cfef124a1e50828e2ad3
JHertz5 <jukka.hertzog@live.co.uk>
2024-01-01 17:24:28 +0000

    #4 Replcaed checklist class with a module that uses item_database and recipe_database.

--- shopping_list/report/checklist.py
+++ shopping_list/report/checklist.py
@@ -1,41 +1,77 @@
 from datetime import datetime
+from shopping_list import item_database
+from shopping_list import recipe_database
+
+
+def generate_timestamp_filename():
+    now = datetime.now()
+    return_string = "shoppinglist_" + now.strftime('%Y%m%d%H%M%S') + ".txt"
+    return return_string
+
+
+def generate_file(filename, recipe_quantity_dict, item_dict, item_grouping_method):
+    write_string_list = []
+
+    # Add recipes to file.
+    for recipe_name, recipe_quantity in recipe_quantity_dict.items():
+        recipe_str = '{} ({})'.format(recipe_name, recipe_quantity)
+        line_string =_generate_line(recipe_str, 'recipes')
+        write_string_list.append(line_string)
+
+    # Add items to the file.
+    for item_name, item_obj in item_dict.items():
+        item_str = '{} ({})'.format(item_name, item_obj.get_quantity())
+        item_group = item_obj.get_group(item_grouping_method)
+        line_string =_generate_line(item_str, item_group)
+        write_string_list.append(line_string)
+
+    # Write the lines to the file.
+    file = open(filename, 'w')
+    file.writelines(write_string_list)
+    file.close()
+
+
+def _generate_line(text, group, tag_list=[]):
+    # Validate inputs.
+    assert isinstance(text, str)
+    assert isinstance(group, str)
+    assert isinstance(tag_list, list)
+
+    tag_list_formatted = [' +' + tag for tag in tag_list]
+    return_string = text + " @" + group + "".join(tag_list_formatted) + '\n'
+    return return_string
+
 
 class Checklist:
 
     def __init__(self, filename=''):
         if filename == '':
             # generate filename
-            now = datetime.now()
-            self.filename = "shoppinglist_" + now.strftime('%Y%m%d%H%M%S') + ".txt"
+            generate_timestamp_filename()
         else:
-            self.filename = filename
+            self._filename = filename
 
-        self.file = open(self.filename,'w')
+    def get_filename(self):
+        return self._filename
 
-    def generate_file(self, shopping_list_grouped):
-        groups = list(shopping_list_grouped.keys())
+    def generate_file(self, recipe_quantity_dict, item_dict, item_grouping_method):
 
-        # add recipes to file
-        if 'recipes' in groups:
-            groups.remove('recipes')
-            for recipe in shopping_list_grouped['recipes']:
-                self.add_line(recipe,'recipes')
+        self.file = open(self._filename, 'w')
 
-        # add unknown groups to file
-        if 'unknown' in groups:
-            groups.remove('unknown')
-            for item in shopping_list_grouped['unknown']:
-                self.add_line(item['name'],'none')
+        # Add recipes to file.
+        for recipe_name, recipe_quantity in recipe_quantity_dict.items():
+            recipe_str = '{} ({})'.format(recipe_name, recipe_quantity)
+            self.write_line(recipe_str, 'recipes')
 
-        # add items to the file
-        for group_idx,group in enumerate(sorted(groups)):
-            for item in shopping_list_grouped[group]:
-                entry = '{} ({})'.format(item['name'], item['num_portions'])
-                self.add_line(entry, str(group_idx))
+        # Add items to the file.
+        for item_name, item_obj in item_dict.items():
+            item_str = '{} ({})'.format(item_name, item_obj.get_quantity())
+            item_group = item_obj.get_group(item_grouping_method)
+            self.write_line(item_str, item_group)
 
         self.file.close()
 
-    def add_line(self, text, group, tag_list=[]):
+    def write_line(self, text, group, tag_list=[]):
         # Validate inputs.
         assert isinstance(text, str)
         assert isinstance(group, str)
