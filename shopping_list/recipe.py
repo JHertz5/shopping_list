6d6c1b49eed23eb9cfa150006d0e227a3c88823c
JHertz5 <jukka.hertzog@live.co.uk>
2024-01-01 14:54:33 +0000

    #4 Modified Recipe class. Added quantity attribute and method to increment quantity. Renamed ingredients_list to items_list to be more conssitent with other code. Removed unused __str__ method.

--- shopping_list/recipe.py
+++ shopping_list/recipe.py
@@ -6,19 +6,20 @@ class Recipe:
     Args:
     - name (str): The name of the recipe.
     - url (str): The URL of the recipe.
-    - ingredients_list (list[str]): A list of ingredients for the recipe.
+    - items_list (list[str]): A list of items (i.e. ingredients) for the recipe.
     '''
 
-    def __init__(self, name, ingredients_list, url=''):
+    def __init__(self, name, items_list, url=''):
         # Validate and assign initial attribute values.
         assert isinstance(name, str)
         # TODO name unused?
         self._name = name
-        assert isinstance(ingredients_list, list)
-        self._ingredients_list = ingredients_list
+        assert isinstance(items_list, list)
+        self._items_list = items_list
         # TODO url is currently unused. Remove?
         assert isinstance(url, str)
         self._url = url
+        self._quantity = 0
 
     def get_name(self):
         return self._name
@@ -26,11 +27,14 @@ class Recipe:
     def get_url(self):
         return self._url
 
-    def get_ingredients_list(self):
-        return self._ingredients_list
+    def get_items_list(self):
+        return self._items_list
 
-    def is_ingredient_in_recipe(self, ingredient):
-        return ingredient in self._ingredients_list
+    def is_item_in_recipe(self, item):
+        return item in self._items_list
 
-    def __str__(self):
-        return "TODO"
+    def incr_quantity(self):
+        self._quantity += 1
+
+    def get_quantity(self):
+        return self._quantity
