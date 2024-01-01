7efba3c4f3453f398a084b4d26839dce27279499
JHertz5 <jukka.hertzog@live.co.uk>
2024-01-01 17:30:54 +0000

    #4 Renamings in preview.

--- shopping_list/report/preview.py
+++ shopping_list/report/preview.py
@@ -2,13 +2,13 @@ from shopping_list import item_database
 from shopping_list import recipe_database
 
 
-def generate_preview(recipe_quantity_dict, item_quantity_dict):
+def generate_report(recipe_quantity_dict, item_quantity_dict):
     recipe_strings = ['{} ({})'.format(x, recipe_quantity_dict[x]) for x in recipe_quantity_dict.keys()]
     item_strings = ['{} ({})'.format(x, item_quantity_dict[x]) for x in item_quantity_dict.keys()]
 
-    preview_string = '\nRecipes:\n\t' \
+    return_string = '\nRecipes:\n\t' \
         + '\n\t'.join(recipe_strings) \
         + '\nItems:\n\t' \
         + '\n\t'.join(item_strings) \
         + '\n'
-    return preview_string
+    return return_string
