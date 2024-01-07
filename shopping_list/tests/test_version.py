96d7f9b68756359d9f0e81274d5ba6a7113d33db
JHertz5 <jukka.hertzog@live.co.uk>
2024-01-07 19:24:04 +0000

    Revert "#73 Removed version unit test."
    This reverts commit c4f219e934e963401482ee50ca593cc0e0bb97ea.

---
+++ shopping_list/tests/test_version.py
@@ -0,0 +1,12 @@
+import unittest
+import subprocess
+
+from shopping_list import version
+
+expected_version_string = (subprocess.check_output(['git', 'describe', '--tags', '--abbrev=0'])).strip().decode('utf-8')
+
+
+class TestVersion(unittest.TestCase):
+
+    def test_version_string(self):
+        self.assertEqual(expected_version_string, version.version_string)
