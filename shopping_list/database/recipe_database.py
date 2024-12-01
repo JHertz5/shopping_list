1048f4551e5369f025471f05a68d0a84ae5d283d
JHertz5 <jukka.hertzog@live.co.uk>
2024-12-01 11:55:27 +0000

    #110 Removed unused method.

--- shopping_list/database/recipe_database.py
+++ shopping_list/database/recipe_database.py
@@ -10,12 +10,6 @@ class RecipeDatabase(ItemDatabase):
     def insert(self, recipe_name, ingredient_list=[]):
         self._item_dict[recipe_name] = recipe.Recipe(ingredient_list=ingredient_list)
 
-    def get_ingredient_list_of_selected(self):
-        return_list = []
-        for name in self.get_names_of_selected():
-            return_list += self._item_dict[name].ingredient_list
-        return return_list
-
     def get_recipes_containing_ingredient(self, ingredient):
         return_list = [x for x in self._item_dict.keys() if self._item_dict[x].ingredient_is_in_recipe(ingredient)]
         return return_list
