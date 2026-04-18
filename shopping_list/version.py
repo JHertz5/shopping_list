6534b0ffd73817bde9263ed67462e9f3aa2eaa42
JHertz5 <jukka.hertzog@live.co.uk>
2026-04-18 17:01:26 +0100

    #133 Increased line length.

--- shopping_list/version.py
+++ shopping_list/version.py
@@ -54,6 +54,7 @@ def reporting_from_git_repo():
 def reporting_from_zip_file():
     return not reporting_from_git_repo()
 
+
 if __name__ == '__main__':
     # Print simple version number when called directly.
     print(version_string)
